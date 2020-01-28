1afe08cd4b061f1376addfabcd47303148d26cdb
mr andersen <chasea523@gmail.com>
2020-01-28 01:19:59 -0800

    added CFonts package for initial start of app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+const CFonts = require('cfonts');
 
 const connectionProperties = {
     host: "localhost",
@@ -15,6 +16,17 @@ const connection = mysql.createConnection(connectionProperties);
 connection.connect((err) => {
     if (err) throw err;
 
-    console.log("\n WELCOME TO EMPLOYEE TRACKER \n");
+    CFonts.say('Employee|Tracker!', {
+        font: 'block',              // define the font face
+        align: 'left',              // define text alignment
+        colors: ['system'],         // define all colors
+        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1,           // define letter spacing
+        lineHeight: 1,              // define the line height
+        space: true,                // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0',             // define how many character can be on one line
+    });
     
+    //TODO: add startup function
 });
+
